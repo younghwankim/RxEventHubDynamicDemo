0e77313e7a7ada139ebf015ce29d7bc347d528df
Chen Shenghan <zzdjk6@gmail.com>
2017-05-23 13:13:12 +0800

    Initial Commit

---
+++ RxEventHubDynamicDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxEventHubDynamicDemo
+//
+//  Created by 陈圣晗 on 23/05/2017.
+//  Copyright © 2017 RxEventHubDynamicDemo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
